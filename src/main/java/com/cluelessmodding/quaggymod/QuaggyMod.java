b7006e01c97644c880d949a6e1088d032169c11c
Clueless-Programming <kuzundr@gmail.com>
2025-05-15 21:12:17 -0700

    Initial commit

---
+++ src/main/java/com/cluelessmodding/quaggymod/QuaggyMod.java
@@ -0,0 +1,15 @@
+package com.cluelessmodding.quaggymod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class QuaggyMod implements ModInitializer {
+	public static final String MOD_ID = "quaggymod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/cluelessmodding/quaggymod/QuaggyModClient.java
@@ -0,0 +1,10 @@
+package com.cluelessmodding.quaggymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class QuaggyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
